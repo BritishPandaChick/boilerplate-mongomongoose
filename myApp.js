7edc57aa84be03a179d916cd5b91e284c499e0f2
BritishPandaChick <sbartley1080@gmail.com>
2020-01-22 17:12:39 -0500

    Add exercise one. Install mongoose and mongodb

--- myApp.js
+++ myApp.js
@@ -7,6 +7,9 @@
 /*  ================== */
 
 /** 1) Install & Set up mongoose */
+var mongodb = require("mongodb");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Add mongodb and mongoose to the project's package.json. Then require 
 // mongoose. Store your Mongo Atlas database URI in the private .env file 
